ce5842249f1711df014b146af4fa8b06a2f18f8e
Ogban Ugot <ogbanugot@gmail.com>
2018-04-06 13:12:10 +0100

    Update the repo with source code Remove extra source code folder

--- Source code/2015_private_knn.py
+++ Source code/2015_private_knn.py
@@ -19,30 +19,41 @@ from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+# =============================================================================
+# Import the dataframe
+# =============================================================================
+
+dataset_train = pd.read_csv('2015_Private.csv')
 
 
+#Features and output for training
+X = dataset_train.iloc[:, [1,3,4,5,6,7]].values #Individually select the  columns
+y = dataset_train.iloc[:, 12].values
 
-dataset = pd.read_csv('../data/xtry4.csv')
-X = dataset.iloc[:, 2:8].values
-y = dataset.iloc[:, 12].values
 
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
+labelencoder_X1 = LabelEncoder()
+X[:, 0] = labelencoder_X1.fit_transform(X[:, 0])
 
-labelencoder_X_2 = LabelEncoder()
-X[:, 3] = labelencoder_X_2.fit_transform(X[:, 3])
+labelencoder_X2 = LabelEncoder()
+X[:, 2] = labelencoder_X2.fit_transform(X[:, 2])
 
-labelencoder_X_2 = LabelEncoder()
-X[:, 4] = labelencoder_X_2.fit_transform(X[:, 4])
+labelencoder_X3 = LabelEncoder()
+X[:, 3] = labelencoder_X3.fit_transform(X[:, 3])
+
+labelencoder_X4 = LabelEncoder()
+X[:, 4] = labelencoder_X4.fit_transform(X[:, 4])
 
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
@@ -54,32 +65,42 @@ X_test = sc.transform(X_test)
 # Fit regression model
 n_neighbors = 10
 weights = "distance"
-knn = neighbors.KNeighborsRegressor(n_neighbors, weights = weights, algorithm='ball_tree', n_jobs =-1)
-
-#Bagging Ensemble 
-from sklearn.ensemble import BaggingRegressor
-
-bagging = BaggingRegressor(knn, max_samples=0.5, max_features=0.5)
-
 kfold = KFold(n_splits=5, shuffle=False, random_state= None)
-
-#bagging.fit(X_train, y_train)
-
+knn = neighbors.KNeighborsRegressor(n_neighbors, weights = weights, algorithm='ball_tree', n_jobs =-1)
 knn.fit(X_train, y_train)
 
-score = cross_val_score(estimator = knn, X  = X_train, y = y_train, cv = kfold, scoring='neg_mean_squared_log_error')
+# =============================================================================
+# #Bagging Ensemble 
+# from sklearn.ensemble import BaggingRegressor
+# bagging = BaggingRegressor(knn, max_samples=0.5, max_features=0.5)
+# kfold = KFold(n_splits=5, shuffle=False, random_state= None)
+# bagging.fit(X_train, y_train)
+# =============================================================================
 
+# =============================================================================
+# Grid search 
+# =============================================================================
+parameters = {'n_neighbors':[5,10],'algorithm':('ball_tree','kd_tree')}
+knn = neighbors.KNeighborsRegressor(weights = 'distance')
+reg = GridSearchCV(knn, parameters, scoring = 'r2', n_jobs = -1)
+reg.fit(X_train, y_train)
+best_score = reg.best_score_
+best_param = reg.best_params_
+best_model = reg.best_estimator_ #Can use in Cross_val_score & predict
+
+#Cross validated estimate on training and test data
+score = cross_val_score(estimator = knn, X  = X_train, y = y_train, cv = kfold, scoring='neg_mean_squared_log_error')
 prediction = cross_val_predict(knn, X_test, y_test, cv= kfold, n_jobs=-1)
 
+#Variants of scoring
+msle = mean_squared_log_error(y_test, prediction)
+mse = mean_squared_error(y_test,prediction)
+mae = mean_absolute_error(y_test,prediction)
+r2 = r2_score(y_test,prediction)
 from math import sqrt
-sqrt(mse)
+sqrt(mse) #root mean --
 
-MSLE = mean_squared_log_error(y_test, prediction)
-MSE = mean_squared_error(y_test,prediction)
-MAE = mean_absolute_error(y_test,prediction)
-R2 = r2_score(y_test,prediction)
-
-#Visualization
+#mode Visualization
 plt.plot(y_test, color = 'red', label = 'Real time lapse')
 plt.plot(prediction, color = 'blue', label = 'Predicted time lapse')
 plt.title('Time lapse interval predictions')
@@ -87,4 +108,3 @@ plt.xlabel('Frequency')
 plt.ylabel('Time lapse')
 plt.legend()
 plt.show()
-
--- Source code/2015_private_svm.py
+++ Source code/2015_private_svm.py
@@ -18,23 +18,35 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import GridSearchCV
 
+s = svm.SVR
+# =============================================================================
+# Import the dataframe
+# =============================================================================
 
-dataset = pd.read_csv('../data/xtry4.csv')
-X = dataset.iloc[:, 2:8].values
+dataset = pd.read_csv('2015_private.csv')
+X = dataset.iloc[:, [2,3,4,5,6,7]].values #Individually select the  columns
 y = dataset.iloc[:, 12].values
 
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
+labelencoder_X = LabelEncoder()
+# =============================================================================
+# X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# 
+# =============================================================================
+labelencoder_X = LabelEncoder()
+X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
 
-labelencoder_X_2 = LabelEncoder()
-X[:, 3] = labelencoder_X_2.fit_transform(X[:, 3])
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 
-labelencoder_X_2 = LabelEncoder()
-X[:, 4] = labelencoder_X_2.fit_transform(X[:, 4])
+labelencoder_X = LabelEncoder()
+X[:, 4] = labelencoder_X.fit_transform(X[:, 4])
 
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
@@ -54,29 +66,39 @@ X_test = sc.transform(X_test)
 svm = svm.SVR()
 kfold = KFold(n_splits=5, shuffle=False, random_state= None)
 
+# =============================================================================
+# #Bagging Ensemble 
+# from sklearn.ensemble import BaggingRegressor
+# bagging = BaggingRegressor(knn, max_samples=0.5, max_features=0.5)
+# kfold = KFold(n_splits=5, shuffle=False, random_state= None)
+# bagging.fit(X_train, y_train)
+# =============================================================================
 
-#Bagging Ensemble 
-from sklearn.ensemble import BaggingRegressor
+# =============================================================================
+# Grid search 
+# =============================================================================
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+svm = svm.SVR()
+reg = GridSearchCV(svm, parameters, scoring ='r2', n_jobs = -1)
+reg.fit(X_train, y_train)
+best_score = reg.best_score_
+best_param = reg.best_params_
+best_model = reg.best_estimator_ #Can use in Cross_val_score & predict
 
 
-bagging = BaggingRegressor(svm, max_samples=0.5, max_features=0.5, n_jobs=-1)
-
-kfold = KFold(n_splits=5, shuffle=False, random_state= None)
-
-bagging.fit(X_train, y_train)
-
+#Cross validated estimate on training and test data
 score = cross_val_score(estimator = svm, X  = X_train, y = y_train, cv = kfold, scoring='neg_mean_squared_log_error')
-
 prediction = cross_val_predict(svm, X_test, y_test, cv= kfold, n_jobs=-1)
 
-mse = mean_squared_log_error(y_test, prediction)
-
-mse2 = mean_squared_error(y_test,prediction)
-
+#Variants of scoring
+msle = mean_squared_log_error(y_test, prediction)
+mse = mean_squared_error(y_test,prediction)
+mae = mean_absolute_error(y_test,prediction)
+r2 = r2_score(y_test,prediction)
 from math import sqrt
-sqrt(mse2)
-#Visualization
-#Visualization
+sqrt(mse) #root mean --
+
+#model Visualization
 plt.plot(y_test, color = 'red', label = 'Real time lapse')
 plt.plot(prediction, color = 'blue', label = 'Predicted time lapse')
 plt.title('Time lapse interval predictions')
@@ -84,4 +106,3 @@ plt.xlabel('Frequency')
 plt.ylabel('Time lapse')
 plt.legend()
 plt.show()
-
